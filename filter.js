049f1108e769663013c261956b513427152513cd
Daniel Castro <castrodd@gmail.com>
2017-08-14 21:06:30 -0400

    Solution to Filtered JS

---
+++ filter.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+
+function find_files() {
+  fs.readdir(process.argv[2], function callback(err, list) {
+    for (i=0; i < list.length; i++) {
+      if (list[i].endsWith("." + process.argv[3])) {
+        console.log(list[i]);
+      }
+    }
+  })
+}
+
+find_files()
