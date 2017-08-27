d35bf0d0b5d127a1ae64e1b3d26a0823fce2da8c
Daniel Castro <castrodd@gmail.com>
2017-08-26 21:49:15 -0400

    Http.get()

---
+++ http.js
@@ -0,0 +1,13 @@
+var http = require('http')
+
+url = process.argv[2]
+
+http.get(url, function callback(response) {
+  response.setEncoding('utf8');
+  response.on("data", function(data) {
+    console.log(data);
+  })
+  response.on("error", function() {
+    console.log("Error!");
+  })
+})
