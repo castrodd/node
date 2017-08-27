94bfb57e34dd554bcd63933eaa5a5f007d052999
Daniel Castro <castrodd@gmail.com>
2017-08-26 21:59:28 -0400

    Read stream

--- all.js
+++ all.js
@@ -1,15 +1,21 @@
-// Prints to the console data from an HTTP GET request
+// Prints to the console all the data from an HTTP GET request
 
 var http = require('http')
 
 url = process.argv[2]
 
 http.get(url, function callback(response) {
+  var stream = '';
   response.setEncoding('utf8');
   response.on("data", function(data) {
-    console.log(data);
+    stream += data;
   })
   response.on("error", function() {
     console.log("Error!");
+  })
+  response.on("end", function() {
+    console.log(stream.length);
+    console.log(stream);
+    return
   })
 })
